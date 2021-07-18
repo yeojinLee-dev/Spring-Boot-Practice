08f923a7762248d70346c008d8f009dd42b5f5d4
yeojinLee-dev <yeojinLee-dev@gmail.com>
2021-07-18 21:29:27 +0900

    커뮤니티 게시판 뷰 구성

---
+++ Spring-Boot-Community-Web/src/main/java/com/web/domain/User.java
@@ -0,0 +1,52 @@
+package com.web.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class User implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @Builder
+    public User(String name, String password, String email, LocalDateTime createdDate,
+                LocalDateTime updatedDate) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
+}
